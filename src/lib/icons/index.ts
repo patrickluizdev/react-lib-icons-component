6b71d2b81dcbba053fad6bddf7619ce57d582403
patrickluizdev <patrick@fasters.com.br>
2024-05-10 14:48:09 -0300

    ✨feat: added base icons library

---
+++ src/lib/icons/index.ts
@@ -0,0 +1,7 @@
+export { Bell } from './bell'
+export { Chevron } from './chevron'
+export { Close } from './close'
+export { Find } from './find'
+export { ShopingBag } from './shoping-bag'
+export { Star } from './star'
+export { Wifi } from './wifi'
