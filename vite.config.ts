f9ef05c9a110f8764f768dcce8e5f076e2d83697
patrickluizdev <patrick@fasters.com.br>
2024-05-10 15:42:06 -0300

    :package: build: added packages for vite build and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  resolve: {
+    extensions: [
+      '.mjs',
+      '.js',
+      '.jsx',
+      '.ts',
+      '.tsx',
+      '.json',
+      '.sass'
+    ]
+  },
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      entry: './src/lib/index.ts',
+      formats: ['es', 'cjs'],
+      name: 'react-lib-icons-component',
+      fileName: (format) => `react-lib-icons-component.${format}.js`
+    }
+  }
 })
