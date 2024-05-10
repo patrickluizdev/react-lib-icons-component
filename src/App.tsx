b58a6843f7ffdda0d01ea28e2d84169f5a20e6b1
patrickluizdev <patrick@fasters.com.br>
2024-05-10 14:40:34 -0300

    :construction: chore: added components on display

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Bell, Chevron, Close, Find, ShopingBag, Star, Wifi } from "./lib"
+
+function App() {
+  return (
+    <>
+      <Bell/>
+      <Chevron/>
+      <Close/>
+      <Find/>
+      <ShopingBag/>
+      <Star/>
+      <Wifi/>
+    </>
+  )
+}
+
+export default App
