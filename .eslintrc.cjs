325bf7f081cd638785b2a6c1e100053b610b0d47
Juan Maciel <juanrodriguesmaciel1@gmail.com>
2024-05-12 22:38:37 +0100

    🔧 feat: project  base structure using some eslint rules, prettier rules and husky first implementation to be improved durign process

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs', '.eslintignore', '.gitignore', '.lintstagedrc.json'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'no-unused-vars': [
+			error,
+			{
+				vars: 'all',
+				args: 'after-used',
+				ignoreRestSiblings: true,
+				argsIgnorePattern: '^_',
+			},
+		],
+		'react/react-in-jsx-scope': 'off',
+		'no-console': ['warn', { allow: ['clear', 'info', 'error', 'dir', 'trace'] }],
+	},
+};
